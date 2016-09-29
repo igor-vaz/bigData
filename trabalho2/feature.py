86cbe034557589948a49461250bf3299981cdc25
Igor Vaz <ivaz567@hotmail.com>
2016-09-29 09:55:48 -0300

    extraindo dados e calculando a media de tempo

--- trabalho2/feature.py
+++ trabalho2/feature.py
@@ -1,17 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import re
 
-data =[]
-oneType = 0
-twoTypes = 0
 
 #leitura de arquivo
-arq = open('amostra_tempo.csv','r')
-arq = arq.read()
-lines = arq.split("\n")
+def readFile(file):
+  data =[]
+  lines = file.split("\n")
 
-for lines in lines[1:]:
-  data.append(lines.split(","))
+  for lines in lines[1:]:
+    data.append(lines.split(";"))
+  return data
 
-print(data[0])
+def sum(_list):
+  res = 0.0
+  for l in _list:
+    res += float(l[1])
+  return res
+
+file = open('dados/amostra_tempo.csv','r')
+file = file.read()
+sample = readFile(file)
+del sample[len(sample)-1]
+
+file = open('dados/populacao_tempo.csv','r')
+file = file.read()
+pop = readFile(file)
+del pop[len(pop)-1]
+
+sum_sample = sum(sample)
+sum_pop = sum(pop)
+
+print("Media de tempo da amostra: "+str(sum_sample/float(len(sample))))
+print("Media de tempo da populacao: "+str(sum_pop/float(len(pop))))
+
+
+# print(len(sample))
+# print(sample)
+# print(len(pop))
+# print(pop[-1])
+
